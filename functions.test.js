08f8a49985a4c2a5da219fd409e6c8885185668c
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-19 13:55:09 -0700

    completed greetings test

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,17 @@
 // const { TestWatcher } = require('@jest/core')
-const {returnTwo} = require('./functions')
+const {returnTwo, greeting} = require('./functions');
 
 test('Should return equal [int]', () => {
     expect(returnTwo(2)).toEqual('integer is 2')});
+
+
+describe('matching names to greetings', () => {
+
+    test('Should return greeting [name]', () => {
+        expect(greeting('James')).toEqual('Hello, James')
+    });
+
+    test('Should return greeting [name]', () => {
+        expect(greeting('Jill')).toEqual('Hello, Jill')
+    });
+});
