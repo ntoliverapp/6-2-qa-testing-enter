f441cfb4436fd6d5aec20a42ba57fd35d1d6c60e
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-19 14:36:24 -0700

    passed subtraction, multiplication, and division

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,5 @@
 // const { TestWatcher } = require('@jest/core')
-const {returnTwo, greeting, add} = require('./functions');
+const {returnTwo, greeting, add, mult, div, subt} = require('./functions');
 
 test('Should return equal [int]', () => {
     expect(returnTwo(2)).toEqual('integer is 2')});
@@ -16,6 +16,8 @@ describe('matching names to greetings', () => {
     });
 });
 
+
+
 describe('integers to equal sum', () => {
 
     test('Should add [numOne] and [numTwo]', () => {
@@ -24,5 +26,33 @@ describe('integers to equal sum', () => {
 
     test('Should add [numOne] and [numTwo]', () => {
         expect(add(5, 9)).toEqual(14)
+    });
+});
+
+describe('integers to multiply', () => {
+
+    test('Should multiply [numOne] and [numTwo]', () => {
+        expect(mult(2, 4)).toEqual(8)
+    });
+
+    test('Should multiply [numOne] and [numTwo]', () => {
+        expect(mult(4, 4)).toEqual(16)
+    });
+});
+describe('integers to divide', () => {
+
+    test('Should divide [numOne] and [numTwo]', () => {
+        expect(div(9, 3)).toEqual(3)
+    });
+    test('Should divide [numOne] and [numTwo]', () => {
+        expect(div(12, 2)).toEqual(6)
+    });
+});
+describe('integers to subtract', () => {
+    test('Should subtract [numOne] and [numTwo]', () => {
+        expect(subt(9, 5)).toEqual(4)
+    });
+    test('Should subtract [numOne] and [numTwo]', () => {
+        expect(subt(44, 5)).toEqual(39)
     });
 });
