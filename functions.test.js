f0fbed01e962ec0f2fc8c193db6057e8275ebf0d
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-19 14:00:08 -0700

    passed add test

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,5 @@
 // const { TestWatcher } = require('@jest/core')
-const {returnTwo, greeting} = require('./functions');
+const {returnTwo, greeting, add} = require('./functions');
 
 test('Should return equal [int]', () => {
     expect(returnTwo(2)).toEqual('integer is 2')});
@@ -13,5 +13,16 @@ describe('matching names to greetings', () => {
 
     test('Should return greeting [name]', () => {
         expect(greeting('Jill')).toEqual('Hello, Jill')
+    });
+});
+
+describe('integers to equal sum', () => {
+
+    test('Should add [numOne] and [numTwo]', () => {
+        expect(add(1, 2)).toEqual(3)
+    });
+
+    test('Should add [numOne] and [numTwo]', () => {
+        expect(add(5, 9)).toEqual(14)
     });
 });
